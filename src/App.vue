be8367557a4b8f723efcab7d1ad5a7dfc8175283
wupeiwen <javapeiwen2010@gmail.com>
2019-03-06 14:01:36 +0800

    feature: 单独使用deck.gl GeoJsonLayer的demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <canvas id="app"></canvas>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import {
+    Deck
+  } from '@deck.gl/core';
+  import {
+    GeoJsonLayer
+  } from '@deck.gl/layers';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    data() {
+      return {
+        deck: ''
+      }
+    },
+    methods: {
+      deck_geojson: function () {
+        const GEOJSON =
+          'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_1_states_provinces_shp.geojson';
+        const INITIAL_VIEW_STATE = {
+          latitude: 35,
+          longitude: -115,
+          zoom: 3,
+          bearing: 30,
+          pitch: 30
+        };
+        this.deck = new Deck({
+          canvas: 'app',
+          initialViewState: INITIAL_VIEW_STATE,
+          controller: true,
+          layers: [
+            new GeoJsonLayer({
+              visible: true,
+              data: GEOJSON,
+              stroked: true,
+              filled: true,
+              lineWidthMinPixels: 2,
+              opacity: 0.4,
+              getLineColor: () => [255, 100, 100],
+              getFillColor: () => [200, 160, 0, 180]
+            })
+          ]
+        });
+      },
+      deckInit: function () {
+        this.deck_geojson()
+      },
+      deckDestory() {
+        this.deck.finalize()
+      }
+    },
+    mounted() {
+      this.deckInit()
+    },
+    beforeDestroy() {
+      this.deckDestory()
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
 </style>
